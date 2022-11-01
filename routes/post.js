d492e17a393a3fcb34124ef9dd9297b6156a0a33
Shako Kinyamba <shakokinyamba201@gmail.com>
2022-11-01 10:35:24 +0200

    Feat:create function to like and dislike

--- routes/post.js
+++ routes/post.js
@@ -6,8 +6,8 @@ router.post('/new_post', postController.new_post)
 router.put('/:id/update_post', postController.update_post)
 router.delete('/:id/delete_post/', postController.delete_post)
 router.patch('/:id/like_post', postController.like_post)
-router.get("/:id", postController.getOnepost);
-router.get('/', postController.getTimeLinePost)
+router.get("/:id", postController.getOne);
+router.get('/timeline/all', postController.getTimeLinePost)
 
 
 module.exports = router
